3a9cbf41949498339c434e3a0d85188a0e2d6781
João Trybus <92412023+JLTryb@users.noreply.github.com>
2022-09-02 16:11:30 -0300

    Word Cloud
    Code created via JupyterNotebook

---
+++ WordCloud.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+get_ipython().system('pip install wordcloud')
+get_ipython().system('pip install fileupload')
+get_ipython().system('pip install ipywidgets')
+get_ipython().system('jupyter nbextension install --py --user fileupload')
+get_ipython().system('jupyter nbextension enable --py fileupload')
+
+import wordcloud
+import numpy as np
+from matplotlib import pyplot as plt
+from IPython.display import display
+import fileupload
+import io
+import sys
+
+def _upload():
+
+    _upload_widget = fileupload.FileUploadWidget()
+
+    def _cb(change):
+        global file_contents
+        decoded = io.StringIO(change['owner'].data.decode('utf-8'))
+        filename = change['owner'].filename
+        print('Uploaded `{}` ({:.2f} kB)'.format(
+            filename, len(decoded.read()) / 2 **10))
+        file_contents = decoded.getvalue()
+
+    _upload_widget.observe(_cb, names='data')
+    display(_upload_widget)
+
+_upload()
+
+def calculate_frequencies(file_contents):
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    uninteresting_words = ["the", "a", "to", "if", "is", "it", "of", "and", "or", "an", "as", "i", "me", "my",     "we", "our", "ours", "you", "your", "yours", "he", "she", "him", "his", "her", "hers", "its", "they", "them",     "their", "what", "which", "who", "whom", "this", "that", "am", "are", "was", "were", "be", "been", "being",     "have", "has", "had", "do", "does", "did", "but", "at", "by", "with", "from", "here", "when", "where", "how",     "all", "any", "both", "each", "few", "more", "some", "such", "no", "nor", "too", "very", "can", "will", "just", "in", "for","there", "then", "on", "would"]    
+    list1 = ""
+    file_contents = list1.join((letter if letter.isalpha() else " ") for letter in file_contents).lower().split()
+    file_contents_new  = [word for word in file_contents if word not in uninteresting_words]
+    WordCount = {word: file_contents.count(word) for word in file_contents_new}
+
+    cloud = wordcloud.WordCloud()
+    cloud.generate_from_frequencies(WordCount)
+    return cloud.to_array()
+
+myimage = calculate_frequencies(file_contents)
+plt.imshow(myimage, interpolation = 'nearest')
+plt.axis('off')
+plt.show()
